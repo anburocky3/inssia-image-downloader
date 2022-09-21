82d0eb28d108917d3f36919deffb75a9dff608fa
Anbu <15264938+anburocky3@users.noreply.github.com>
2022-09-21 19:08:40 +0530

    init

---
+++ README.md
@@ -0,0 +1,36 @@
+# Forum Image Grabber (Inssia)
+
+Simple tool, which will download all images based on the URL provided.
+
+> Some of the elements found on the below platform contains +18 content and if you are under-aged, STOP IT. Its not for you.
+## Screenshots
+
+## How to use it?
+
+1. [Fork the repository]() and run the `main.py` using the following command
+
+```shell
+python main.py # you have to have python 3 installed.
+```
+
+2. It will ask you which directory, output folder name etc. input those information correctly.
+
+| Inputs  | Sample | DataType |
+   | ------------- | ------------- | ----|
+| - Enter your **URL**, where you want to grab the images:  | Any Post thread link from inssia.com   | _string_ |
+| - In Which **directory**, you want to put the images?  | Appropriate folder title | _string_  |
+| - From which page to grab **START**? (default: 0):  | Any numeric value or skip (defaults to '0') | _integer_ |
+| - Till which page to grab: **END**?  | Any numeric value | _integer_ |
+| - Page Per Items count (default: 10):  | Any numeric value or skip (defaults to '10') | _integer_ |
+
+3. It will download it for you. Automatically.
+
+### License: MIT + (use it with care)
+
+### Tested platforms
+
+- inssia.com
+
+### Authors & Maintainer
+
+- [Anbuselvan Rocky](https://fb.me/anburocky3)
---
+++ main.py
@@ -0,0 +1,75 @@
+import os
+import requests
+import shutil
+from bs4 import BeautifulSoup
+from termcolor import colored
+
+output_dir = 'output'
+
+
+def createOutputFolderIfNotExist():
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+
+def welcomePage():
+    url_to_grab = input("Enter your URL, where you want to grab the images:\t")
+    user_dir = input('In Which directory, you want to put the images?\t')
+    start_num = int(input('From which page to grab START? (default: 0):\t') or '0')
+    end_num = int(input('Till which page to grab: END?\t'))
+    per_page_item = int(input('Page Per Items count (default: 10):\t') or '10')
+
+    directory = f'{output_dir}/{user_dir}'
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    pages = []
+
+    for i in range(start_num, end_num, per_page_item):
+        pages.append(i + per_page_item)
+
+    for page in pages:
+        url = f'{url_to_grab}&start=' + str(page)
+        print(f' + {colored("Getting", "green")} this URL: {colored(url, "blue")}')
+        spider(url, directory)
+
+
+def spider(url, directory):
+    image_count = 1
+
+    # while page <= max_pages:
+    # url = 'https://www.inssia.com/viewtopic.php?f=35&t=23XXX&start=' + str(page)
+    sourcecode = requests.get(url)
+    plaintext = sourcecode.text
+    soup = BeautifulSoup(plaintext, "lxml")
+
+    for tag in soup.findAll('img', {"class": "postimage"}):
+        link = tag.get('src')  # get the link
+
+        # Check if the tag is in expect format
+        # del tag['src']
+        # if tag.attrs != {';': '', 'alt': '', 'border': '0'}:
+        #     continue
+
+        filename = link.strip('/').rsplit('/', 1)[-1]  # to get the correct file name
+
+        res = requests.get(link, stream=True)  # use requests to get the content of the images
+
+        if res.status_code == 200:
+            with open(f'{directory}/{filename}', 'wb') as f:
+                shutil.copyfileobj(res.raw, f)
+                # f.write(image)  # write the image into a file
+                print(
+                    f'{colored(f"    ---#{image_count} SUCCESS:", "green")} - Image successfully Downloaded: {colored(filename, "blue")}')
+
+            image_count += 1
+        else:
+            print(f'{colored("    ---ERROR:", "red")} - Image Could not be retrieved')
+
+    print(f'Total Images found on {url} is: {colored(image_count, "orange")}')
+
+
+if __name__ == '__main__':
+    createOutputFolderIfNotExist()
+    welcomePage()
