0f531c64f164ee4647c9da9692b4ebdbc1c27999
aneesh08192 <92361323+aneesh08192@users.noreply.github.com>
2022-10-18 00:08:24 +0530

    add multithreading
    Default 100 threads can min or max based on your comforts

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import os
 import requests
-import shutil
 from bs4 import BeautifulSoup
 from termcolor import colored
+from urllib3.exceptions import InsecureRequestWarning
+import concurrent.futures
+import subprocess
 
+global directory
 output_dir = 'output'
+finalimages = []
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
 }
 
-
 def createOutputFolderIfNotExist():
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-
 def welcomePage():
+    global user_dir
+
     url_to_grab = input("Enter your URL, where you want to grab the images:\t")
     user_dir = input('In Which directory, you want to put the images?\t')
     start_num = int(input('From which page to grab START? (default: 0):\t') or '0')
@@ -29,52 +34,41 @@ def welcomePage():
         os.makedirs(directory)
 
     pages = []
-
+    urls = []
     for i in range(start_num, end_num, per_page_item):
         pages.append(i + per_page_item)
 
     for page in pages:
         url = f'{url_to_grab}&start=' + str(page)
-        print(f' + {colored("Getting", "green")} this URL: {colored(url, "blue")}')
-        spider(url, directory)
+        urls.append(url)
 
+    with concurrent.futures.ThreadPoolExecutor(100) as executor: # Adjust Threads as per your network and PC config
+        executor.map(spider, urls)
+    
 
-def spider(url, directory):
-    image_count = 1
-
-    # while page <= max_pages:
+def spider(links):
     # url = 'https://www.inssia.com/viewtopic.php?f=35&t=23XXX&start=' + str(page)
-    sourcecode = requests.get(url, headers=headers)
+    sourcecode = requests.get(links, headers=headers, verify=False, timeout=None)
     plaintext = sourcecode.text
     soup = BeautifulSoup(plaintext, "lxml")
 
+    img_count = 0
     for tag in soup.findAll('img', {"class": "postimage"}):
+        img_count += 1
         link = tag.get('src')  # get the link
+        finalimages.append(link)
+    print(colored(f"Current working on {links} and found {img_count}", "yellow"))
 
-        # Check if the tag is in expect format
-        # del tag['src']
-        # if tag.attrs != {';': '', 'alt': '', 'border': '0'}:
-        #     continue
-
-        filename = link.strip('/').rsplit('/', 1)[-1]  # to get the correct file name
-
-        res = requests.get(link, headers=headers, stream=True)  # use requests to get the content of the images
-
-        if res.status_code == 200:
-            with open(f'{directory}/{filename}', 'wb') as f:
-                shutil.copyfileobj(res.raw, f)
-                # f.write(image)  # write the image into a file
-                print(
-                    f'{colored(f"    ---#{image_count} SUCCESS:", "green")}'
-                    f' - Image successfully Downloaded: {colored(filename, "blue")}')
-
-            image_count += 1
-        else:
-            print(f'{colored("    ---ERROR:", "red")} - Image Could not be retrieved: {colored(filename, "blue")}')
-
-    print(colored(f"Total Images found on {url} is: {image_count}", "yellow"))
-
+def imgdownloader():
+    cmd = ".\\aria2c.exe -x 16 -i actress.txt -d output\\"+ user_dir +"\\"
+    subprocess.call(cmd, shell=True) 
 
 if __name__ == '__main__':
+    file = open('actress.txt','w')
     createOutputFolderIfNotExist()
     welcomePage()
+    for img in finalimages:
+        file.write(img+'\n')
+    file.close()
+    imgdownloader()
+    
